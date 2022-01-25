2ffba992285f1e6cc15f2ed8627e69a416effada
Andrea Camargo <andreabyq1991@gmail.com>
2022-01-25 18:07:34 -0400

    New project app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from flask import flask
+from flask import Flask
+
 app = Flask(__name__)
-@app.route(/)
+
+@app.route("/")
 def home_view():
-    return "<h1> Welcome to my new app</h1>"
+		return "<h1>Welcome to Geeks for Geeks</h1>"
+
     

9aae7b3c09703fb494aacba5909031cf2a576251
Simbelmyne <simbelmyne.evermind.131717@gmail.com>
2022-03-29 18:37:33 -0400

    ftoc function web app

---
+++ app/function.py
@@ -0,0 +1,2 @@
+def ftoc(ftemp):
+   return (ftemp-32.0)*(5.0/9.0)
--- app/main.py
+++ app/main.py
@@ -1,27 +1,23 @@
 from flask import Flask
-from flask import request
-
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    celsius = request.args.get("celsius", "")
-    return (
-        """<form action="" method="get">
-                <input type="text" name="celsius">
-                <input type="submit" value="Convert">
-            </form>"""
-        + celsius
-    )
+def hello():
+    return "Hello World!"
+def ftoc(ftemp):
+   return (ftemp-32.0)*(5.0/9.0)
 
-@app.route("/<int:celsius>")
-def fahrenheit_from(celsius):
-    """Convert Celsius to Fahrenheit degrees."""
-    fahrenheit = float(celsius) * 9 / 5 + 32
-    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-    return str(fahrenheit)
+@app.route('/ftoc/<ftempString>')
+def convertFtoC(ftempString):
+    ftemp = 0.0
+    try:
+        ftemp = float(ftempString)
+        ctemp = ftoc(ftemp)
+        return "Farenheit: " + ftempString + " Celsius " + str(ctemp) 
+    except ValueError:
+        return "Sorry.  Could not convert " + ftempString + " to a number"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(port=5000)
 
     

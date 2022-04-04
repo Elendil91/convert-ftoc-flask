e8ee9d1c023434dff18375560e94563462921af2
Simbelmyne <simbelmyne.evermind.131717@gmail.com>
2022-04-03 21:13:50 -0400

    first commit

--- app/main.py
+++ app/main.py
@@ -1,23 +1,43 @@
-from flask import Flask
+import os
+from flask import Flask, url_for, render_template, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def render_main():
+    return render_template('home.html')
+
+@app.route('/ctof')
+def render_ctof():
+    return render_template('ctof.html')
+
+@app.route('/ftoc')
+def render_ftoc():
+    return render_template('ftoc.html')
+
+@app.route('/ftoc_result')
+def render_ftoc_result():
+    try:
+        ftemp_result = float(request.args['fTemp'])
+        ctemp_result = ftoc(ftemp_result)
+        return render_template('ftoc_result.html', fTemp=ftemp_result, cTemp=ctemp_result)
+    except ValueError:
+        return "Sorry: something went wrong."
+
+@app.route('/ctof_result')
+def render_ctof_result():
+    try:
+        ctemp_result = float(request.args['cTemp'])
+        ftemp_result = ctof(ctemp_result)
+        return render_template('ctof_result.html', cTemp=ctemp_result, fTemp=ftemp_result)
+    except ValueError:
+        return "Sorry: something went wrong."
+
 def ftoc(ftemp):
    return (ftemp-32.0)*(5.0/9.0)
 
-@app.route('/ftoc/<ftempString>')
-def convertFtoC(ftempString):
-    ftemp = 0.0
-    try:
-        ftemp = float(ftempString)
-        ctemp = ftoc(ftemp)
-        return "Farenheit: " + ftempString + " Celsius " + str(ctemp) 
-    except ValueError:
-        return "Sorry.  Could not convert " + ftempString + " to a number"
+def ctof(cTemp):
+   return (cTemp * (9/5) + 32)
 
-if __name__ == "__main__":
-    app.run(port=5000)
-
-    
+if __name__=="__main__":
+    app.run(debug=True, port=54321)   
